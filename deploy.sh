61fd538130ac6802400b08d8935e5bd3ffd367e3
Stefan <84-stefancruz@users.noreply.gitlab.futo.org>
2025-01-16 21:53:29 +0000

    fix: update sourceUrl for pre-releases

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,14 @@ PRE_RELEASE=${PRE_RELEASE:-false}  # Default to false if not set
 
 # Determine deployment directory
 if [ "$PRE_RELEASE" = "true" ]; then
-    DEPLOY_DIR="$DOCUMENT_ROOT/pre-release/Odysee"
+    RELATIVE_PATH="pre-release/Odysee"
 else
-    DEPLOY_DIR="$DOCUMENT_ROOT/Odysee"
+    RELATIVE_PATH="Odysee"
 fi
+
+DEPLOY_DIR="$DOCUMENT_ROOT/$RELATIVE_PATH"
+PLUGIN_URL_ROOT="https://plugins.grayjay.app/$RELATIVE_PATH"
+SOURCE_URL="$PLUGIN_URL_ROOT/OdyseeConfig.json"
 
 # Take site offline
 echo "Taking site offline..."
@@ -21,6 +25,17 @@ mkdir -p "$DEPLOY_DIR"
 cp OdyseeIcon.png "$DEPLOY_DIR"
 cp OdyseeConfig.json "$DEPLOY_DIR"
 cp OdyseeScript.js "$DEPLOY_DIR"
+
+# Update the sourceUrl in OdyseeConfig.json
+echo "Updating sourceUrl in OdyseeConfig.json..."
+jq --arg sourceUrl "$SOURCE_URL" '.sourceUrl = $sourceUrl' "$DEPLOY_DIR/OdyseeConfig.json" > "$DEPLOY_DIR/OdyseeConfig_temp.json"
+if [ $? -eq 0 ]; then
+    mv "$DEPLOY_DIR/OdyseeConfig_temp.json" "$DEPLOY_DIR/OdyseeConfig.json"
+else
+    echo "Failed to update OdyseeConfig.json" >&2
+    exit 1
+fi
+
 sh sign.sh "$DEPLOY_DIR/OdyseeScript.js" "$DEPLOY_DIR/OdyseeConfig.json"
 
 # Notify Cloudflare to wipe the CDN cache
@@ -28,8 +43,8 @@ echo "Purging Cloudflare cache for zone $CLOUDFLARE_ZONE_ID..."
 curl -X POST "https://api.cloudflare.com/client/v4/zones/$CLOUDFLARE_ZONE_ID/purge_cache" \
      -H "Authorization: Bearer $CLOUDFLARE_API_TOKEN" \
      -H "Content-Type: application/json" \
-     --data '{"files":["https://plugins.grayjay.app/Odysee/OdyseeIcon.png", "https://plugins.grayjay.app/Odysee/OdyseeConfig.json", "https://plugins.grayjay.app/Odysee/OdyseeScript.js"]}'
+     --data '{"files":["'"$PLUGIN_URL_ROOT/OdyseeIcon.png"'", "'"$PLUGIN_URL_ROOT/OdyseeConfig.json"'", "'"$PLUGIN_URL_ROOT/OdyseeScript.js"'"]}'
 
 # Take site back online
 echo "Bringing site back online..."
-rm $DOCUMENT_ROOT/maintenance.file
+rm "$DOCUMENT_ROOT/maintenance.file"
